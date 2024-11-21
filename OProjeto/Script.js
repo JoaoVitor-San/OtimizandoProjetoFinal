a31e148e6d32a824a3f9313e53a4a7bcc925c828
João Vitor Santana <joao.sant.dev@gmail.com>
2024-11-20 23:20:33 -0300

    OtimizaçãoProjeto

--- OProjeto/Script.js
+++ OProjeto/Script.js
@@ -1,17 +1,15 @@
-let numero1 = parseFloat(prompt("Digite o primeiro número:"));
-let numero2 = parseFloat(prompt("Digite o segundo número:"));
-let operacao = prompt("Digite a operação que deseja realizar (soma, subtração, multiplicação, divisão):");
-function soma(a, b) {
-    return a + b;
-}
-function subtracao(a, b) {
-    return a - b;
-}
-function multiplicacao(a, b) {
-    return a * b;
-}
+// Cria um objeto com os números e operação
+const input = {
+    numero1: parseFloat(prompt("Digite o primeiro número:")),
+    numero2: parseFloat(prompt("Digite o segundo número:")),
+    operacao: prompt("Digite a operação que deseja realizar (soma, subtração, multiplicação, divisão):"),
+};
 
-}
+const { numero1, numero2, operacao } = input;
+
+const resultado = operacoes[operacao]
+    ? operacoes[operacao](numero1, numero2)
+    : "Operação inválida!";
+
 alert("O resultado da operação é: " + resultado);
 console.log("O resultado da operação é:", resultado);
-
